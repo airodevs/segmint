55333b44a6143952c86b9d85fd1c58d8931fa6fa
airodevs <93663920+airodevs@users.noreply.github.com>
2021-11-04 12:55:00 +0000

    Initial util.php commit

---
+++ util.php
@@ -0,0 +1,21 @@
+<?php
+
+function segmint(&name, ...$args)
+{
+	$arguments = array();
+	
+	foreach ($args as &argument)
+	{
+		array_push($arguments, $argument);
+	}
+	
+	try 
+	{
+		include("/" + $name)
+	} catch()
+	{
+		echo("Segmint not found! Check the name is spelled correctly and that the segmint file is in the proper location.")
+	}
+}
+
+?>
