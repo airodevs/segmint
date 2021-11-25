8554eefd7d173be58d18d7a3e2d56dfe97c9411b
airodevs <93663920+airodevs@users.noreply.github.com>
2021-11-25 11:31:28 +0000

    Lang changes

--- config.php
+++ config.php
@@ -11,9 +11,35 @@ return $config = array(
     "db_host" => "",
 
     "sitename" => "",
+    "lang" => "en",
 
     "installed" => false;
 );
 
+function getLang()
+{
+    if ($config["lang"] == "en")
+    {
+        return $lang_en;
+    } elseif ($config["lang"] == "de")
+    {
+        return $lang_de;
+    } elseif ($config["lang"] == "cy")
+    {
+        return $lang_cy;
+    }
+}
+
+$lang_en = array(
+    "login" => "Log in",
+);
+
+$lang_de = array(
+    "" => ,
+);
+
+$lang_cy = array(
+    "" => ,
+);
 
 ?>
